35b14445b1d8f91c6f0eb1be90bef311ca94dea2
Leonardo Daniel López López <lopez_lopez_daniel@hotmail.com>
2022-12-20 11:04:07 -0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:360,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
